00ff702004f597782fc9162531b4d8255b96f934
Rostmaster <rotmaster007@gmail.com>
2024-03-15 17:32:19 +0200

    Inintial preparation done

---
+++ index.js
@@ -0,0 +1,22 @@
+import knex from 'knex';
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import path from 'path';
+import config from 'config'
+
+import logger from './logger/logger.js';
+
+const port  = config.server.port;
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '/static/')));
+
+app.listen(port, () => {
+    console.clear();
+    logger.info(`Server started on port ${port}`)
+})
+
+const data_base = knex(config.database)
